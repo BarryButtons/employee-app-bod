da9990e3cd88df2b62895422dc06f39690f6c3d2
Blah <blah@gmail.com>
2022-10-06 19:46:15 +0100

    Change bonus to Percentage of Gross
    Net Pay calculation

--- src/main/kotlin/ie/setu/main.kt
+++ src/main/kotlin/ie/setu/main.kt
@@ -9,7 +9,7 @@ val employeeID = 6143
 val grossSalary = 67543.21
 val payePercentage = 38.5
 val prsiPercentage = 5.2
-val annualBonus = 1450.50
+val annualBonus = 8.2
 val cycleToWorkMonthlyDeduction = 54.33
 
 fun main(args: Array<String>){
@@ -23,10 +23,11 @@ fun roundTwoDecimals(number: Double) = round(number * 100) / 100
 fun printPayslip() {
 
     val monthlySalary = roundTwoDecimals(grossSalary / 12)
+    val totalBonus = roundTwoDecimals(grossSalary * (annualBonus / 100))
     val monthlyPrsi = roundTwoDecimals(monthlySalary * (prsiPercentage / 100))
     val monthlyPaye = roundTwoDecimals(monthlySalary * (payePercentage / 100))
-    val grossPay = roundTwoDecimals((monthlySalary + (annualBonus / 12)))
-    val totalDeductions = roundTwoDecimals((monthlyPrsi + monthlyPrsi + cycleToWorkMonthlyDeduction))
+    val grossPay = roundTwoDecimals((monthlySalary + (totalBonus / 12)))
+    val totalDeductions = roundTwoDecimals((monthlyPaye + monthlyPrsi + cycleToWorkMonthlyDeduction))
     val netPay = roundTwoDecimals((grossPay - totalDeductions))
 
     println("_________________________________________________________________________")
@@ -40,7 +41,7 @@ fun printPayslip() {
     println("|    PAYMENT DETAILS \t\t\t\t DEDUCTION DETAILS  \t\t\t\t|")
     println("|_______________________________________________________________________|")
     println("|    Salary: $monthlySalary \t\t\t\t PAYE: $monthlyPaye \t\t\t\t\t\t|")
-    println("|    Bonus:  ${roundTwoDecimals(annualBonus/12)}\t\t\t\t\t PRSI: $monthlyPrsi \t\t\t\t\t\t|")
+    println("|    Bonus:  ${roundTwoDecimals(totalBonus/12)}\t\t\t\t\t PRSI: $monthlyPrsi \t\t\t\t\t\t|")
     println("|    \t\t\t\t\t\t\t\t Cycle To Work: " + cycleToWorkMonthlyDeduction + "\t\t\t\t|")
     println("|_______________________________________________________________________|")
     println("|    Gross:  $grossPay  \t\t\t\tTotal Deductions: $totalDeductions \t\t\t|")
